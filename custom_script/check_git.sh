84ac9b70e557303ec14189c65ec6d6b459779449
Martono Wibowo <martonowibowo@gmail.com>
2016-05-17 11:20:43 +0700

    add array to check git modified

--- custom_script/check_git.sh
+++ custom_script/check_git.sh
@@ -6,10 +6,16 @@ file=$2
 
 if result=$(cd ${path} && git status | grep modified | awk '{print "ls -lah '${path}'/"$3}');then
 echo "$result" > /tmp/result.${file} 2>&1 
-sh /tmp/result | awk '{print $5" "$7" "$6" "$8" "$9}'
+sh /tmp/result.${file} | awk '{print $5" "$7" "$6" "$8" "$9}'
 else
 echo nothing modified
 fi
 }
-path_folder=$1
-check /opt/public_html/${path_folder} ${path_folder}
+#path_folder=$1
+#check /opt/public_html/${path_folder} ${path_folder}
+files=($(ls /opt/public_html/))
+for item in ${files[*]}
+do
+echo $item 
+check /opt/public_html/$item $item
+done
