b5e10b99ad7f3d914082516fdcd2bb3d9de3c563
Martono Wibowo <martonowibowo@gmail.com>
2016-05-18 19:26:52 +0700

    fix change concat and add variabel

--- custom_script/check_access_path.sh
+++ custom_script/check_access_path.sh
@@ -1,13 +1,13 @@
-
 #!/bin/bash
-parsepath=$(cat palomino_access.log  | awk '!/index.php/ &&! /.jpg/ && ! /.png/ && !/.JPEG/ && !/.JPG/ && !/.js/ && !/.css/ && !/.gif/ && !/.woff/ && !/.ttf/ {print $6"-"$7"-"$1}'|sort)
+accesslog=$1
+parsepath=$(cat ${accesslog}  | awk '!/index.php/ &&! /.jpg/ && ! /.png/ && !/.JPEG/ && !/.JPG/ && !/.js/ && !/.css/ && !/.gif/ && !/.woff/ && !/.ttf/ {print $6"|"$7"|"$1}'|sort)
 echo "$parsepath" > /tmp/parsepath
 tmp=/tmp/parsepath
 pathlist=$(cat $tmp|sort |uniq)
 for path in ${pathlist[*]}
 do
 crot=$(cat $tmp | grep $path |sort -nr|wc -l)
-if [ "$crot" -gt "50" ]
+if [ "$crot" -gt "10" ]
 then
 echo "$crot : $path"
 fi
