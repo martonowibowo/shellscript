9368368598463ed84cb687ae35f10a208d8aafe0
Martono Wibowo <martonowibowo@gmail.com>
2017-09-07 17:47:13 +0700

    Update add_zone.sh

--- flashee/add_zone.sh
+++ flashee/add_zone.sh
@@ -3,8 +3,8 @@ domain_name=$1
 if echo "${domain_name}" | grep -q "acomindo"
   then
     if result=$(curl -X POST "https://api.cloudflare.com/client/v4/zones/59d02e76694ffce8068b0164bc3caca1/dns_records" \
-    -H "X-Auth-Email: ferdian.robianto@acommerce.asia" \
-    -H "X-Auth-Key: 5c70a8bb0973f324559716718edd7c4984c85" \
+    -H "X-Auth-Email: " \
+    -H "X-Auth-Key: " \
     -H "Content-Type: application/json" \
     --data '{"type":"A","name":"'$domain_name'","content":"103.23.21.224","ttl":600}' | grep null);then
       echo '{"error":true,"message":"DOMAIN ADD FAILED"}'
